887e09901496ad26b0dc00d7c03df2b14406b0ff
BenMiller0 <bem002@ucsd.edu>
2026-03-30 22:18:33 -0700

    create indivual thread for each LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,67 @@
 #include <Arduino.h>
 
-#define LED_PIN13 13
-#define LED_PIN26 26
-#define LED_PIN25 25
-#define LED_PIN33 33
-#define LED_PIN32 32
-#define LED_PIN27 27
 
-int LEDS[] = {LED_PIN13, LED_PIN26, LED_PIN25, LED_PIN33, LED_PIN32, LED_PIN27};
+// GPIO Pin Constants, named after the LED location
+#define L_BELT_RED 13
+#define L_BELT_GREEN_0 27
+#define L_BELT_GREEN_1 26
+#define R_BELT_RED 25
+#define R_BELT_GREEN_0 33
+#define R_BELT_GREEN_1 32
+int LEDS[] = {L_BELT_RED, L_BELT_GREEN_0, L_BELT_GREEN_1, R_BELT_RED, R_BELT_GREEN_0, R_BELT_GREEN_1};
 
-void blinkTask(void *pvParameters) {
+
+// This is the struct that will be passed in as the params to each thread created for each LED
+struct LedTaskParams {
+    int pin;
+    int delay;
+};
+
+
+// Definition of thread that is created for each LED
+void ledBlinkTask(void *pvParameters) {
+    LedTaskParams* params = (LedTaskParams*)pvParameters;
     while (true) {
-        for (int pin : LEDS) {
-            digitalWrite(pin, HIGH);
-        }
-        vTaskDelay(500 / portTICK_PERIOD_MS);  //
-        for (int pin : LEDS) {
-            digitalWrite(pin, LOW);
-        }
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        digitalWrite(params->pin, HIGH);
+        vTaskDelay(params->delay / portTICK_PERIOD_MS);
+        digitalWrite(params->pin, LOW);
+        vTaskDelay(params->delay / portTICK_PERIOD_MS);
     }
 }
 
+
 void setup() {
+    // Set each LED GPIO pun as output pin
     for (int pin : LEDS) {
         pinMode(pin, OUTPUT);
     }
 
-    xTaskCreate(
-        blinkTask,    // Task function
-        "BlinkTask",  // Task name
-        1000,         // Stack size
-        NULL,         // Parameters
-        1,            // Priority
-        NULL          // Task handle
-    );
+    // Parameter arrays (must persist for task lifetime)
+    static LedTaskParams ledParams[6] = {
+        {L_BELT_RED, 200},
+        {L_BELT_GREEN_0, 400},
+        {L_BELT_GREEN_1, 600},
+        {R_BELT_RED, 800},
+        {R_BELT_GREEN_0, 1000}, 
+        {R_BELT_GREEN_1, 1200}  
+    };
+
+    // Create separate thread for each LED
+    for (int i = 0; i < 6; i++) {
+        char taskName[20];
+        
+        xTaskCreate(
+            ledBlinkTask, 
+            taskName,
+            1000,
+            &ledParams[i], 
+            1,
+            NULL
+        );
+    }
 }
 
+
 void loop() {
-    // Empty - FreeRTOS takes over
+    // Empty, FreeRTOS threads take over
 }
